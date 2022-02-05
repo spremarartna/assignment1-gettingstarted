6deec6e9c5e9ad7ff9d097d62fae27a3f433372f
spremarartna <95597829+spremarartna@users.noreply.github.com>
2022-02-04 22:11:23 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,24 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+  if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    	answer = "mtls"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No"
+    	answer = "No"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No"
+    	answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+    	answer == "65fc2f78620c22cc1f483c7d7201b592" #this is with the appostraphes 
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No"
+    	answer == "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    	answer = "4"
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+    	answer = "3"
     return(answer)
 # Complete all the questions.
 
